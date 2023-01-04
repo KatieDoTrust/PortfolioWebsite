c6471337b4dd3f468bdf1c1cbdbeec3f2919dcd5
KatieDoTrust <118731357+KatieDoTrust@users.noreply.github.com>
2023-01-04 08:53:34 +0000

    toastify is working

--- src/EmailForm.js
+++ src/EmailForm.js
@@ -1,4 +1,5 @@
-import {toast} from 'react-toastify'
+import {toast, ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 import emailjs from '@emailjs/browser'
 import {useRef} from 'react'
 
@@ -10,27 +11,29 @@ function EmailForm() {
     emailjs.sendForm("service_zoj329x", "contact_form", form.current, "y0DfgYROnru8eEtuD")
     .then(
       () => {
-        toast.success('Message successfully sent!')
+        toast('Message successfully sent!')
         window.setTimeout(() => {
           window.location.reload()
-        }, 1000)
+        }, 3000)
       },
       () => {
-        toast.error('Failed to send the message, please try again')
+        toast('Failed to send the message, please try again')
       }
     )
-
   }
 
 
   return (
+    <>
+    <ToastContainer/>
     <form ref={form} onSubmit={sendEmail} className='email-form'>
     <input type="text" name="user_name" placeholder='Your Name' className='email-form-input'
     />
     <input type="text" name="user_email" placeholder='Your Email Address' className='email-form-input'/>
     <textarea type="text" name="message" placeholder='Your Message' className='email-form-message'/>
     <button type="submit" value="Send" className='email-button'>Send Email</button>
-  </form>
+    </form>
+    </>
   )
 }
 
